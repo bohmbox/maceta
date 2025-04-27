4511303239f3884d4f707475001925837358b69f
MarcosPabri <marcossamuel.1d@gmail.com>
2025-04-27 16:32:15 -0600

    Actualización archivos, además añadí el archivo proyecto word

--- Código/Sensor_T_y_H/Sensor_T_y_H.ino
+++ Código/Sensor_T_y_H/Sensor_T_y_H.ino
@@ -2,7 +2,8 @@
 #include <ThingSpeak.h>
 #include <DHT.h>
 
-#define sensor 21
+#define DHT_PIN 21
+#define LDR_PIN 33
 
 const char* ssid= "INFINITUM8B31";
 const char* pass = "SzELAh5mt3";
@@ -11,7 +12,7 @@ unsigned long idch = 2901429;
 const char* Wapikey = "SEW4IK5RMINOGQRL";
 
 WiFiClient Client;
-DHT dht1 (sensor,DHT11);
+DHT dht1 (DHT_PIN,DHT11);
 
 void setup() {
   //put your setup code here, to run once: 
@@ -28,6 +29,7 @@ void loop() {
   //put your main code here, to run repeatedly;
   float t = dht1.readTemperature();
   float h = dht1.readHumidity();
+  int ldrValue = analogRead(LDR_PIN);
 
   while (isnan(t) || isnan(h)) {
     Serial.println("Falla sensor DHT11...");
@@ -43,9 +45,14 @@ void loop() {
   Serial.print(h);
   Serial.println(" $.");
 
+  Serial.print("Intensidad de luz: ");
+  Serial.print(ldrValue);
+  Serial.println("(valor analógico)");
+
   Serial.println("......................................");
   ThingSpeak.setField (1,t);
   ThingSpeak.setField (2,h);
+  ThingSpeak.setField (3,ldrValue);
   ThingSpeak.writeFields(idch,Wapikey);
   Serial.println("Enviados");
   delay(14000);
